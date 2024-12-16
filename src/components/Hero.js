a6064562a7369c8ed8c9fbbef85a95e6d342849d
egeatmaca <egeatmaca98@gmail.com>
2024-12-16 12:06:05 +0100

    network graph updated

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -4,7 +4,7 @@ import NetworkGraph from './NetworkGraph';
 function Hero() {
     return (
       <div id="home" class='hero'>
-        <h3>Hi , I’m</h3>
+        <h3>Hi, I’m</h3>
         <h1>Ege</h1>
         <div>
           I build Data / ML products tailored for domain-specific applications.
--- src/components/NetworkGraph.js
+++ src/components/NetworkGraph.js
@@ -1,6 +1,7 @@
 import React, { useRef, useEffect } from "react";
 import * as d3 from "d3";
 
+
 const NetworkGraph = () => {
   const canvasRef = useRef(null);
 
@@ -8,18 +9,33 @@ const NetworkGraph = () => {
     // Example dataset
     const data = {
       nodes: [
-        { id: "Python", group: 1 },
-        { id: "Machine Learning", group: 1 },
-        { id: "Deep Learning", group: 1 },
-        { id: "SQL", group: 3 },
-        { id: "Data Analysis", group: 3 },
+        { id: "I'm here!", group: 0 },
+        { id: "Data Analytics", group: 1 },
+        { id: "BI", group: 1 },
+        { id: "ML", group: 1 },
+        { id: "NLP", group: 1 },
+        { id: "Software Development", group: 5 },
+        { id: "Domain Knowledge", group: 6 },
+        { id: "Product", group: 6 },
+        { id: "Marketing", group: 6 },
+        { id: "Operations", group: 6 },
       ],
       links: [
-        { source: "Python", target: "Machine Learning" },
-        { source: "Python", target: "Deep Learning" },
-        { source: "Machine Learning", target: "Deep Learning" },
-        { source: "SQL", target: "Data Analysis" },
-        { source: "Data Analysis", target: "Machine Learning" }
+        { source: "I'm here!", target: "Data Analytics" },
+        { source: "I'm here!", target: "ML" },
+        { source: "I'm here!", target: "BI" },
+        { source: "I'm here!", target: "Software Development" },
+        { source: "I'm here!", target: "Domain Knowledge" },
+        { source: "Data Analytics", target: "BI" },
+        { source: "Data Analytics", target: "ML" },
+        { source: "Data Analytics", target: "Domain Knowledge" },
+        { source: "Data Analytics", target: "Software Development" },
+        { source: "Domain Knowledge", target: "Product" },
+        { source: "Domain Knowledge", target: "Marketing" },
+        { source: "Domain Knowledge", target: "Operations" },
+        { source: "ML", target: "NLP" },
+        { source: "ML", target: "Software Development" },
+        { source: "BI", target: "Software Development" },
       ]
     };
 
@@ -28,7 +44,7 @@ const NetworkGraph = () => {
     const width = canvas.width;
     const height = canvas.height;
 
-    const color = d3.scaleOrdinal(d3.schemeCategory10);
+    const color = d3.scaleOrdinal(d3.schemeDark2);
 
     const simulation = d3.forceSimulation(data.nodes)
       .force("link", d3.forceLink(data.links).id(d => d.id).distance(100))
@@ -41,9 +57,9 @@ const NetworkGraph = () => {
 
       // Draw links
       context.beginPath();
-      data.links.forEach(drawLink);
-      context.strokeStyle = "#999";
+      context.strokeStyle = "#01a252";
       context.lineWidth = 1.5;
+      data.links.forEach(drawLink);
       context.stroke();
 
       // Draw nodes
@@ -57,14 +73,16 @@ const NetworkGraph = () => {
 
     function drawNode(node) {
       context.beginPath();
-      context.arc(node.x, node.y, 20, 0, 2 * Math.PI);
-      context.fillStyle = color(node.group);
+      context.arc(node.x, node.y, 14, 0, 2 * Math.PI);
+      console.log(node.group);
+      const color_from_scheme = color(node.group);
+      context.fillStyle = node.group == 0 ? '#01a252' : color_from_scheme;
       context.fill();
 
       // Add labels
-      context.font = "12px Arial";
-      context.fillStyle = "#fff";
-      context.fillText(node.id, node.x, node.y);
+      context.font = "18px Arial";
+      context.fillStyle = "#dddddd";
+      context.fillText(node.id, node.x + 14, node.y - 4);
     }
 
     // Cleanup on unmount
@@ -76,8 +94,8 @@ const NetworkGraph = () => {
   return (
     <canvas
       ref={canvasRef}
-      width={400}
-      height={400}
+      width={600}
+      height={600}
     ></canvas>
   );
 };
